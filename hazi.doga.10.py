6976d71e3c3bbd6ff1dfac06b5f277abf98d5e0b
Horvath Máté <horvath.mate.benjamin.tech2020b@bolyaimovar.com>
2021-11-28 22:13:34 +0100

    hazidoga10

---
+++ hazi.doga.10.py
@@ -0,0 +1,5 @@
+t1= [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június',  'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+for i in range(12):
+    lista3 = [t2[i], t1[i]]
+    print (lista3)
