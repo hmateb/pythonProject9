1413c744d9bfbd32a951e7aaffc8aee03eb27119
Horvath Máté <horvath.mate.benjamin.tech2020b@bolyaimovar.com>
2022-02-13 19:20:23 +0100

    Merge remote-tracking branch 'origin/master'

--- orai.munka.py
+++ orai.munka.py
@@ -1,71 +1,44 @@
-print("1.feladat")
-print(30*"*")
+lista = []
+def paros_e():
+    null = False
+    while not null:
+       a = int(input("Adjon meg egy számot: "))
+       if a == 0:
+            null = True
+       else:
+            lista.append(a)
+            if a % 2 == 0:
+                print("Ez a szám páros!")
+            else:
+                print("Ez egy páratlan szám!")
+    print(lista)
 
-n=int(input("Kérem irjon be egy számot"))
-m=int(input("Kérje irjon be egy számot"))
-if n>m:
-    print(n)
-else:
-    print(m)
-
-print("2.feladat")
-a=int(input("Kérje be a kocka egyik oldalának a számát"))
-T=(a*a*a)
-if T<27:
-    print("elég kicsi kocka")
-else:
-    print("Jó nagy kocka")
-
-print(T)
-
-print("3.feladat")
-print(30*"*")
-
-a=int(input("Kérem adjon meg egy életkort"))
-if a<18:
-    print("Nem szavazhat")
-else:
-    print("Szavazhat")
-
-print("4.feladat")
-print(30*"*")
-
-n=int(input("kérem irjon be egy tetszőleges számot"))
-for i in range(1,n):
-    if i%2==0:
-        print(i)
-
-print("5.feladat")
-print(30*"*")
-n=int(input("irjon be egy számot"))
-m=0
-for i in range(0,n):
-    for k in range(0,n):
-        if m == k:
-            print("1",end="")
-        else:
-            print("0",end="")
-    m =m + 1
-    print("")
-
-print("6.feladat")
-print(30*"*")
-a = []
-b=0
-for i in range(1,3):
-    n=int(input("irjon be egy számot"))
-    b=b+n
-    a.append(n)
-print("A számok átlaga",b/len(a))
+def osszeg_fg():
+    b = 0
+    for i in lista:
+        b += i
+    print("A számok összege: ", b)
+def atlag():
+    b=0
+    for i in lista:
+        b += i
+    print("A számok átlaga:",b/len(lista))
 
 
+def harommal_oszthatok():
+    c = 0
+    for i in range(len(lista)):
+        if lista[i]%3==0:
+            c += 1
+    print("3-al osztható számok:",c)
+def kisebb():
+    min=0
+    for i in lista:
+        if lista[i]<lista[0]:
+            min = lista[i]
+    print("legkisebb szám:",min)
 
-print("7.feladat")
-print(30*"*")
-a = []
-m=0
-for i in range(1,11):
-    n=int(input("irjon be egy számot"))
-    m=m+n
-    a.append(n)
-print("A számok átlaga",m/len(a))
+paros_e()
+osszegfg()
+atlag()
+harommal
