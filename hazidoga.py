137bf0b35d3517550bacb346ed8d4f1013e68148
Horvath Máté <horvath.mate.benjamin.tech2020b@bolyaimovar.com>
2022-02-27 14:00:40 +0100

    Merge remote-tracking branch 'origin/master'

--- hazidoga.py
+++ hazidoga.py
@@ -1,18 +1,16 @@
-#afeladat:
-d = input("Adj meg egy szót: ")
-b = []
-for i in range(len(d)):
-    b.append(ord(d[i])-32)
-
-for i in b:
-    print(chr(i), end=" ")
-print(" ")
-#bfeladat
-a = input("Adj meg egy szót: ")
-d = []
-for i in range(len(a)):
-    d.append(ord(a[i])-32)
-
-for i in range(len(d), 0, -1):
-    print(chr(d[i-1]), end=" ")
-print(" ")
+titkositas = 0
+eltolas = 0
+def cezar():
+    a = input("Kérem adjon meg egy szót: ")
+    global titkositas
+    global eltolas
+    eltolas = int(input("Kérem adja meg az eltolás mértékét: "))
+    for i in a:
+        if ord(i) + eltolas > 122 and 97 <= ord(i) <= 122:
+            titkositas += 96 + (ord(i) + eltolas - 122)
+        elif ord(i) + eltolas > 90 and 65 <= ord(i) <= 90:
+            titkositas += 64 + (ord(i) + eltolas - 90)
+        else:
+            titkositas += ord(i) + eltolas
+    print(chr(titkositas))
+cezar()
